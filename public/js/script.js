5b1bb25cf5a2f5ce855a19f5bf99038a89c4dde4
Nailton Vital <nailtonvital35@gmail.com>
2022-07-22 22:17:58 -0300

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/OaIa7CussQ3xPM-I/scene.splinecode');
